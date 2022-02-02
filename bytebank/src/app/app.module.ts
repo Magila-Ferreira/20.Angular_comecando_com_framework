a47d131036f0904925bde2d421b61b6a4e7ac8bc
Mágila-Ferreira <magilamorganasf@hotmail.com>
2022-02-02 14:37:33 -0300

    Organização da aplicação Angular CLI | Criação e utilização de um componente Angular

--- bytebank/src/app/app.module.ts
+++ bytebank/src/app/app.module.ts
@@ -1,16 +1,14 @@
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    /* DECLARATIONS: declara os componentes da aplicação no Módulo */
+    declarations: [AppComponent, NovaTransferenciaComponent],
+    imports: [BrowserModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
