54083425f928ffdf57430de6ec0e3f345b80b11d
Mágila-Ferreira <magilamorganasf@hotmail.com>
2022-02-03 10:36:20 -0300

    03/02_Aula 2 - Importe das funcionalidades Form_Argular: event binding, two-way data binding, property binding

--- bytebank/src/app/app.module.ts
+++ bytebank/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     /* DECLARATIONS: declara os componentes da aplicação no Módulo */
     declarations: [AppComponent, NovaTransferenciaComponent],
-    imports: [BrowserModule],
+    imports: [
+        BrowserModule,
+        /* IMPORTAÇÃO DO MÓDULO_SUBMIT-FORM ANGULAR  */
+        FormsModule
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
--- bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,8 +1,19 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-nova-transferencia',
     templateUrl: './nova-transferencia.component.html',
-    styleUrls: ['./nova-transferencia.component.scss']
+    styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent {}
+export class NovaTransferenciaComponent {
+    // MÉTODO_TRANSFERIR: invocado pelo BOTÃO_SUBMIT do FORM_TRANSFERÊNCIA
+
+    valor: number;
+    destino: string;
+
+    transferir() {
+        console.log('Nova transferência solicitada');
+        console.log("Valor: ", this.valor);
+        console.log("Destino: ", this.destino);
+    }
+}
