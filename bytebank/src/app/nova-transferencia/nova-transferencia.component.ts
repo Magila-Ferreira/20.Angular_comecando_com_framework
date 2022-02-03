d62d978c4b35f7d5519c04e17164d7ef1348a769
Mágila-Ferreira <magilamorganasf@hotmail.com>
2022-02-03 14:34:32 -0300

    03/02_Aula 3 - Exportando dados dos componentes: @Output(), event Binding

--- bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-nova-transferencia',
@@ -8,12 +9,21 @@ import { Component } from '@angular/core';
 export class NovaTransferenciaComponent {
     // MÉTODO_TRANSFERIR: invocado pelo BOTÃO_SUBMIT do FORM_TRANSFERÊNCIA
 
+    @Output() aoTransferir = new EventEmitter<any>();
+
     valor: number;
     destino: string;
 
     transferir() {
         console.log('Nova transferência solicitada');
-        console.log("Valor: ", this.valor);
-        console.log("Destino: ", this.destino);
+
+        const valorEmitir = { valor: this.valor, destino: this.destino };
+        this.aoTransferir.emit(valorEmitir);
+        
+        this.limparCampos();
+    }
+    limparCampos() {
+        this.valor = 0;
+        this.destino = "";
     }
 }
